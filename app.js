1ee57ba2e8a8d4e3e46e285a0249cd4316797f51
BEST <PAWAT_BEST@WINDOWSLIVE.COM>
2024-08-15 15:33:28 +0700

    go

--- app.js
+++ app.js
@@ -19,52 +19,109 @@ global.knex = knex
 app.use(express.urlencoded())
 app.use(express.json({ extended: false }))
 
-const job = schedule.scheduleJob('1 * * * * *', function () {
-    try {
-        let yourDate = new Date()
-        const date2 = new Date(yourDate.getTime() + 5 * 60000);
-        knex('ACTIVITIES')
-            .select('*')
-            .where('start_date', `${yourDate.toISOString().split('T')[0]} ${date2.getHours()}:${date2.getMinutes()}:00`)
-            .then(result => {
-                // let form = new FormData();
-                // form.append('message', `รายงานประจำวันที่ ${new Date().toLocaleDateString('th')} กรุณาตรวจสอบ ${name.toString()} `)
-                if (result.length > 0) {
-                    axios.post('https://api.line.me/v2/bot/message/push', {
-                        "to": result[0]?.user_id,
-                        "messages": [{
-                            "type": "text",
-                            "text": `อีก 5 นาที คุณมี"${result[0]?.topic}"`
-                        }]
-                    }, {
-                        headers: {
-                            Authorization: `Bearer ${process.env.lineToken}`
-                        }
-                    }).then(() => console.log('send msg'))
-                }
+schedule.scheduleJob('0 * * * * *', () => {
+    knex('REMIND')
+        .select('*')
+        // .havingNotNull('day_frequency')
+        .where('hour_frequency', new Date().getHours())
+        .andWhere('minute_frequency', new Date().getMinutes())
+        .andWhere('routine', 1)
+        .orWhere(function() {
+            this.where('hour_frequency', new Date().getHours())
+                .andWhere('minute_frequency', new Date().getMinutes())
+                .andWhere('routine', 0)
+                .andWhere('sended', 0)
+        })
+        .then(result => {
+            result?.map(x => {
+                console.log(x)
+                // axios.post('https://api.line.me/v2/bot/message/push', {
+                //     "to": x?.user_id,
+                //     "messages": [{
+                //         "type": "text",
+                //         "text": x?.topic
+                //     }]
+                // }, {
+                //     headers: {
+                //         Authorization: `Bearer ${process.env.lineToken}`
+                //     }
+                // }).then(() => console.log('send msg'))
             })
-            knex('ACTIVITIES')
-            .select('*')
-            .where('start_date', `${yourDate.toISOString().split('T')[0]} ${yourDate.getHours()}:${yourDate.getMinutes()}:00`)
-            .then(result => {
-                if (result.length > 0) {
-                    axios.post('https://api.line.me/v2/bot/message/push', {
-                        "to": result[0]?.user_id,
-                        "messages": [{
-                            "type": "text",
-                            "text": `คุณมี"${result[0]?.topic}"`
-                        }]
-                    }, {
-                        headers: {
-                            Authorization: `Bearer ${process.env.lineToken}`
-                        }
-                    }).then(() => console.log('send msg'))
-                }
-            })
-    } catch(err) {
-        console.log(err)
-    }
+        })
 })
+
+schedule.scheduleJob('0 0 0 0 0 *', () => {
+    const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+
+    const today = new Date()
+    const dayOfWeek = days[today.getDay()]
+
+    knex('REMIND')
+        .select('*')
+        .whereRaw("frequency NOT REGEXP '^[0-9]+$'")
+        .then(result => {
+            result?.map(x => {
+                if (x.frequency.includes(dayOfWeek)) {
+                    axios.post('https://api.line.me/v2/bot/message/push', {
+                        "to": x?.user_id,
+                        "messages": [{
+                            "type": "text",
+                            "text": x?.topic
+                        }]
+                    }, {
+                        headers: {
+                            Authorization: `Bearer ${process.env.lineToken}`
+                        }
+                    }).then(() => console.log('send msg'))
+                }
+            })
+        })
+})
+
+// const job = schedule.scheduleJob('1 * * * * *', function () {
+//     try {
+//         let yourDate = new Date()
+//         const date2 = new Date(yourDate.getTime() + 5 * 60000);
+//         knex('ACTIVITIES')
+//             .select('*')
+//             .where('start_date_time', `${yourDate.toISOString().split('T')[0]} ${date2.getHours()}:${date2.getMinutes()}:00`)
+//             .then(result => {
+//                 if (result.length > 0) {
+//                     axios.post('https://api.line.me/v2/bot/message/push', {
+//                         "to": result[0]?.user_id,
+//                         "messages": [{
+//                             "type": "text",
+//                             "text": `อีก 5 นาที คุณมี"${result[0]?.topic}"`
+//                         }]
+//                     }, {
+//                         headers: {
+//                             Authorization: `Bearer ${process.env.lineToken}`
+//                         }
+//                     }).then(() => console.log('send msg'))
+//                 }
+//             })
+//         knex('ACTIVITIES')
+//             .select('*')
+//             .where('start_date_time', `${yourDate.toISOString().split('T')[0]} ${yourDate.getHours()}:${yourDate.getMinutes()}:00`)
+//             .then(result => {
+//                 if (result.length > 0) {
+//                     axios.post('https://api.line.me/v2/bot/message/push', {
+//                         "to": result[0]?.user_id,
+//                         "messages": [{
+//                             "type": "text",
+//                             "text": `คุณมี"${result[0]?.topic}"`
+//                         }]
+//                     }, {
+//                         headers: {
+//                             Authorization: `Bearer ${process.env.lineToken}`
+//                         }
+//                     }).then(() => console.log('send msg'))
+//                 }
+//             })
+//     } catch (err) {
+//         console.log(err)
+//     }
+// })
 
 app.listen(3001, () => {
     console.log('start on port 3001')
